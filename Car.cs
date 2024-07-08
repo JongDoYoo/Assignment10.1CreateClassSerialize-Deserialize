901968b41ece502728132dbc8777ae3ed21c8802
JongD <JongD@JONGDO>
2024-07-08 13:55:52 -0700

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10._1CreateClassSerialize_Deserialize
+{
+    internal class Car
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        
+        private bool _isElectric;                   //backing field  if you're going to make get set manual, you need a backing field/ private _name
+        public bool IsElectric
+        {
+            get { return _isElectric; }
+            set { _isElectric = value; }
+        }
+        public double Price { get; set; }
+
+        public Car()   //this is my contructor 
+        {
+            Make = "Tesla";
+            Model = "ModelY";
+            Year = 2024;
+            IsElectric = true;
+            Price = 50000;
+        }
+    }  
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace Assignment10._1CreateClassSerialize_Deserialize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();  //created a distinct datatype to work with
+
+            car1.Make = "Tesla";
+            car1.Model = "ModelY";
+            car1.Year = 2024;
+            car1.IsElectric = true;
+            car1.Price = 50000;
+
+            string car1Json = JsonSerializer.Serialize(car1);
+
+            Console.WriteLine("Serialized Car 1:");
+            Console.WriteLine(car1Json);
+
+            Console.WriteLine();
+
+            Car deserializedCar1 = JsonSerializer.Deserialize<Car>(car1Json);
+            Console.WriteLine("Deserialized Car 1:");
+            Console.WriteLine($"Make: {deserializedCar1.Make}, Model: {deserializedCar1.Model}");
+
+        }
+    }
+}
+
